5482f12fa8a7866e85ff1a307ca7be1aedaa57e3
yejiancong <15900135@qq.com>
2017-05-18 18:07:57 +0800

    ROUTE

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+
+class PagesController
+{
+    public function home()
+    {
+        view('home');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/phpinfo', function () {
     return view('welcome');
 });
+
+
+# ------------------ Page Route ------------------------
+Route::get('/', 'PagesController@home')->name('home');
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/search', 'PagesController@search')->name('search');
+Route::get('/sitemap', 'PagesController@sitemap');
+Route::get('/sitemap.xml', 'PagesController@sitemap');
+
+# ------------------ Admin Route ------------------------
+Route::get('/admin/{vue_capture?}', 'Admin\IndexController@index')->where('vue_capture', '[\/\w\.-]*');
